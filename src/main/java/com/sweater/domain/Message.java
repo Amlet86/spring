ebaa60ef1e5ab0fa7746b47b89758819a89d6e02
andrey <wecow@yandex.ru>
2019-07-03 10:53:23 +0300

    added authorName for each message

--- src/main/java/com/sweater/domain/Message.java
+++ src/main/java/com/sweater/domain/Message.java
@@ -1,26 +1,32 @@
 package com.sweater.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Message {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     private String text;
     private String tag;
 
-    public Message(){
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    public Message() {
     }
 
-    public Message(String text, String tag) {
+    public Message(String text, String tag, User user) {
         this.text = text;
         this.tag = tag;
+        this.author = user;
+    }
+
+    public String getAuthorName(){
+        return author != null ? author.getUsername() : "<none>";
     }
 
     public Integer getId() {
@@ -35,6 +41,10 @@ public class Message {
         return tag;
     }
 
+    public User getAuthor() {
+        return author;
+    }
+
     public void setId(Integer id) {
         this.id = id;
     }
@@ -45,5 +55,10 @@ public class Message {
 
     public void setTag(String tag) {
         this.tag = tag;
+    }
+
+
+    public void setAuthor(User author) {
+        this.author = author;
     }
 }
